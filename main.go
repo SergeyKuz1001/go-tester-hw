2a61b5ef0e3ef152ecce5a80d9076941ed52f31e
Kuzivanov Sergey <rep78398@gmail.com>
2021-09-18 09:10:32 +0200

    Second version

--- main.go
+++ main.go
@@ -1,66 +1,89 @@
 package main
 
 import (
+    "strings"
     "fmt"
     "go/parser"
     "go/token"
     "go/ast"
     "math/rand"
+    "strconv"
+    "os"
 )
 
-func generateString (length int) string {
+type FuncTest []string
+
+type FuncTests []FuncTest
+
+type FileTest map[string]FuncTests
+
+func GenerateString (length int) (str string) {
     n := rand.Intn(length)
-    s := ""
+    str = ""
     for i := 0; i < n; i++ {
-        s += string(rand.Intn(128))
+        str += string(rand.Intn(128))
     }
-    return s
+    return
 }
 
-func generateTests (fun *ast.FuncDecl, testAmt int) (tests [][]interface{}) {
-    tests = make([][]interface{}, testAmt)
+func GenerateFuncTests (testedFunc *ast.FuncDecl, funcTestAmt int) (funcTests FuncTests) {
+    funcTests = make([]FuncTest, funcTestAmt)
     paramTypes := []string{}
-    for _, param := range fun.Type.Params.List {
+    for _, param := range testedFunc.Type.Params.List {
         typeName := param.Type.(*ast.Ident).Name
         for range param.Names {
             paramTypes = append(paramTypes, typeName)
         }
     }
     paramAmt := len(paramTypes)
-    for i := range tests {
-        tests[i] = make([]interface{}, paramAmt)
+    for i := range funcTests {
+        funcTests[i] = make(FuncTest, paramAmt)
         for j := range paramTypes {
             switch paramTypes[j] {
             case "int":
-                tests[i][j] = int(rand.Uint64())
+                funcTests[i][j] = strconv.Itoa(int(rand.Uint64()))
             case "string":
-                tests[i][j] = generateString(42)
+                funcTests[i][j] = fmt.Sprintf("%#v", GenerateString(42))
             default:
-                tests[i][j] = nil
+                funcTests[i][j] = "nil"
             }
         }
     }
     return
 }
 
-func main () {
+func GenerateFileTest (testedFile string, funcTestAmt int) (fileTest FileTest) {
     fset := token.NewFileSet()
-    testFile := "test-files/1.go"
-    file, err := parser.ParseFile(fset, testFile, nil, 0)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+    file, _ := parser.ParseFile(fset, testedFile, nil, 0)
+    fileTest = make(FileTest)
     for _, d := range file.Decls {
         if fun, ok := d.(*ast.FuncDecl); ok {
-            tests := generateTests(fun, 5)
-            for _, test := range tests {
-                fmt.Printf("%s(%#v", fun.Name, test[0])
-                for i := 1; i < len(test); i++ {
-                    fmt.Printf(", %#v", test[i])
-                }
-                fmt.Println(")")
-            }
+            fileTest[fun.Name.Name] = GenerateFuncTests(fun, funcTestAmt)
         }
     }
+    return
+}
+
+func GenerateTestingFile (fileTest FileTest) (testingFile string) {
+    testingFile = "package test1\n\nimport \"testing\""
+    for name, tests := range fileTest {
+        testingFile += "\n\nfunc Test_" + name + " (t *testing.T) {\n"
+        for i, test := range tests {
+            testStr := strings.Join(test, ", ")
+            funcStr := name + "(" + testStr + ")"
+            iStr := strconv.Itoa(i)
+            testingFile += "    t.Run(\"" + iStr + "\", func (t *testing.T) { " + funcStr + " })\n"
+        }
+        testingFile += "}"
+    }
+    return
+}
+
+func main () {
+    testedFile := "test-files/test1.go"
+    fileTest := GenerateFileTest(testedFile, 5)
+    testingFile := GenerateTestingFile(fileTest)
+    file, _ := os.OpenFile("test-files/test1_test.go", os.O_WRONLY|os.O_CREATE, 0644)
+    defer file.Close()
+    file.WriteString(testingFile)
 }
--- test-files/test1.go
+++ test-files/test1.go
@@ -3,3 +3,6 @@ package test1
 func f (x, y int, z string) int {
     return x + 2 * y - len(z)
 }
+
+func g (a string) {
+}
